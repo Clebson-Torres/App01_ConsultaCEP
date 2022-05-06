ca8f08eacfdf83ea4540c6cb97d121178f51fe9e
Clebson Torres <clebson.torres@gmail.com>
2022-05-06 20:20:37 -0300

    Criado Layout

---
+++ App01_ConsultaCEP/App01_ConsultaCEP/servicos/ViaCEPservico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultaCEP.servicos.modelo;
+using Newtonsoft.Json;
+
+
+
+namespace App01_ConsultaCEP.servicos
+{
+    public class ViaCEPservico
+    {
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{}/json/";
+
+        public static endereco BuscarENderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoUrl, cep);
+
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            endereco end = JsonConvert.DeserializeObject<endereco>(Conteudo);
+
+            return end;
+
+        } 
+    }
+}
---
+++ App01_ConsultaCEP/App01_ConsultaCEP/servicos/modelo/endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultaCEP.servicos.modelo
+{
+     public class endereco
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+
+
+
+
+    }
+}
